c408d37963622ee7893a2a66a9fe722a1ea5fbb7
bradyDarren <dcgbfinancial@gmail.com>
2025-04-22 01:40:50 -0500

    creation of the graph as well as the adjustment to the first pixel

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 import requests
 
+PIXELA_TOKEN = 'abcd1234'
+USERNAME = 'darrenb'
+
 PIXELA_URL = 'https://pixe.la/v1/users'
 
+# creation of the pixela user account
 user_params = {
-    'token':'abcd1234',
-    'username':"darrenb",
+    'token':PIXELA_TOKEN,
+    'username':USERNAME,
     'agreeTermsOfService':'yes',
     'notMinor':'yes'
 }
 
-response = requests.post(url=PIXELA_URL, json=user_params)
-print(response.text)
+# response = requests.post(url=PIXELA_URL, json=user_params)
+
+# creation of a graph wiht in our pixela account
+GRAPH_URL = f'{PIXELA_URL}/{USERNAME}/graphs'
+
+graph_params = {
+    'id':'graph1',
+    'name':'Workout Graph',
+    'unit':'minutes',
+    'type':'int',
+    'color':'ajisai',
+    'startOnMonday': True, 
+}
+
+graph_headers = {
+    'X-USER-TOKEN':PIXELA_TOKEN
+}
+
+# graph = requests.post(url=GRAPH_URL, json=graph_params, headers=graph_headers)
+# print(graph.text)
+
+# posting of a pixel onto the above graph
+
+PIXEL_UPDATE_URL = f'{PIXELA_URL}/{USERNAME}/graphs/graph1'
+
+pixel_header = {
+    'X-USER-TOKEN':PIXELA_TOKEN
+}
+
+pixel_posting_params = {
+    'date':'20250420',
+    'quantity':'127',
+}
+
+pixel = requests.post(url=PIXEL_UPDATE_URL, json=pixel_posting_params, headers=pixel_header)
+print(pixel.text)
